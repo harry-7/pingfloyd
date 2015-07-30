f4100a500d4b0bd1d83b342b7dac6fedac11b9a7
Mrinal Dhar <mrinal.dhar@gmail.com>
2015-07-31 04:04:22 +0530

    Built a custom search-results page

---
+++ search.php
@@ -0,0 +1,48 @@
+<?php /* Template Name: SearchResults */ ?>
+
+<html>
+<head>
+<title>Ping!<?php echo wp_title(); ?></title>
+<link rel="stylesheet" href="<?php bloginfo('stylesheet_url'); ?>">
+<style>
+.post_title {
+	font-size: 1.4em;
+}
+.more-link {
+	font-family: gotham;
+	text-transform: uppercase;
+	color: rgba(0,0,0,0.6);
+
+}
+.post_time {
+	margin-bottom: 0px;
+	font-size: 0.8em;
+}
+#content p {
+	font-size: 1em;
+}
+</style>
+</head>
+<body>
+<div id="wrapper">
+	<?php get_header(); ?>
+<div id="main">
+<div id="content">
+	<h1 id="page_title">SEARCH RESULTS</h1>
+	<hr />
+<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+
+<h1 class="post_title"><?php the_title(); ?></h1>
+<h4 class="post_time">Posted on <?php the_time('F jS, Y') ?></h4>
+<p><?php the_content('Continue reading...'); ?></p>
+<hr /> 
+<?php endwhile; else: ?>
+<p><?php _e('Oops! Looks like there are no posts matching that criteria. '); ?></p><?php endif; ?>
+</div>
+<?php get_sidebar(); ?>
+</div>
+<div id="delimiter">
+</div>
+</div>
+</body>
+</html>
