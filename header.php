39b7f1287751f4cfca2a4b134497178ff5dcf4b3
Mrinal Dhar <mrinal.dhar@gmail.com>
2015-07-31 03:16:45 +0530

    Search feature works

--- header.php
+++ header.php
@@ -2,13 +2,13 @@
 	<table id="herobar" width="80%" align="center">
 	<tr>
 		<td width="25%">
-			<span id="logo_hero">Ping!</span>
+			<span id="logo_hero"><a href="<?php bloginfo('url'); ?>">Ping!</a></span>
 		</td>
 		<td width="50%">
 			<table id="navbar" width="100%" align="center">
 				<tr>
 					<td width="25%" class="anim">
-						News &amp; Thought
+					News &amp; Thought
 					</td>
 					<td width="25%" class="anim">
 						Campus Buzz
@@ -28,9 +28,13 @@
 					<td align="right" width="10%">
 			<i id="searchicon" class="material-icons">search</i>
 		</td>
+			<form role="search" method="get" id="searchform" class="searchform" action="<?php echo esc_url( home_url( '/' ) ); ?>">
+
 		<td width="90%">
-			<input type="text" name="searchvalue" id="searchvalue" />
+			<input type="text" name="s" id="searchvalue" />
 		</td>
+		</form>
+
 	</tr></table>
 		</td>
 	</tr>
