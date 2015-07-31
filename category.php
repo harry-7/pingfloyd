6b1dba7001b54000a7522b8018abe728360a2262
Mrinal Dhar <mrinal.dhar@gmail.com>
2015-08-01 01:04:18 +0530

    Made the category page non ugly

--- category.php
+++ category.php
@@ -23,12 +23,12 @@ $args = array( 'numberposts' => '1', 'category' => get_cat_ID(single_cat_title('
 // var_dump($this_category);
 $latest_post = wp_get_recent_posts($args);
 	
-	$colors_main = Array("#B71C1C", "#880E4F", "#4A148C", "#0D47A1", "#006064", "#004D40", "#33691E", "#3E2723");
-	$color = $colors_main[rand(0, 7)];
+	// $colors_main = Array("#B71C1C", "#880E4F", "#4A148C", "#0D47A1", "#006064", "#004D40", "#33691E", "#3E2723");
+	// $color = $colors_main[rand(0, 7)];
 
 foreach( $latest_post as $post ){
 		echo '<a href="' . get_permalink($post["ID"]) . '">';
-		echo '<div class="col-md-12 anim" style="background-color: '. $color . ';" id="cat_topheader"><span class="post_tile">';
+		echo '<div class="col-md-12 anim" id="cat_topheader"><span class="post_tile">';
 		echo $post["post_title"].'</span><span class="author_tile">';
 		// $author = get_the_author($post["ID"]);
 		echo 'by ' . get_the_author_meta('display_name', $post["post_author"]) . ' &bull; ' . get_the_time('F jS, Y') . '</span><span class="tags_tile"><ul class="tags_area">';
@@ -47,7 +47,7 @@ echo '</ul></span></div></a>';
 	<div class="col-md-12">
 		<div class="row">
 			<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
-			<a href="<?php echo the_permalink() ?>"><div class="col-md-4 tile anim" style="background-color: <?php $color = $colors_main[rand(0, 7)]; echo $color; ?>;"><span class="post_tile"><?php the_title(); ?></span><span class="author_tile">by <?php the_author(); ?> &bull; <?php the_time('F jS, Y') ?></span><span class="tags_tile">
+			<a href="<?php echo the_permalink() ?>"><div class="col-md-4 tile anim"><span class="post_tile"><?php the_title(); ?></span><span class="author_tile">by <?php the_author(); ?> &bull; <?php the_time('F jS, Y') ?></span><span class="tags_tile">
 				<?php
 		$tags = get_the_tags();
 				// $tags = get_the_tags();
