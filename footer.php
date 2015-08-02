040a9dc3e285c270b091fbf609339fee10a6f95f
Mrinal Dhar <mrinal.dhar@gmail.com>
2015-08-02 17:35:44 +0530

    Added footer

--- footer.php
+++ footer.php
@@ -1,3 +1,9 @@
 <div id="footer">
-<h1>FOOTER</h1>
+	<table width="100%">
+	<tr>
+		<td width="30%"><a class="anim" href="<?php echo get_page_link( get_page_by_title( 'Team Ping' )->ID ); ?>">About Us</a></td>
+		<td width="30%"><a class="anim" href="#">Editorial Note</a></td>
+		<td width="30%"><a class="anim" href="#">Send us an article</a></td>
+	</tr>
+</table>
 </div>
