289bee552f15aec721e5313da16e423a8d7a0ba4
Mrinal Dhar <mrinal.dhar@gmail.com>
2015-08-19 00:59:09 +0530

    Added Aug 2015 editorial

--- editorial-note.php
+++ editorial-note.php
@@ -35,10 +35,38 @@ a {
 <div id="main">
 <div id="content">
 	<h1 id="page_title">Editorial Note</h1>
+
 	<hr />
 	<p>
-	<br />
-	Coming Soon. 
+
+
+It’s here! It’s here! The all new Ping!!
+</p><p>
+Ping! has always been the byproduct of a group of people that want to put something out there. They want to say something, they want to share something, they want to reach beyond their circle of friends on social media. 
+</p><p>
+
+This year, we have an all new Ping!. New writers, new editors, a new design team, and most importantly new ideas and new enthusiasm. 
+</p><p>
+
+You have an idea? You want to test out the waters of writing? Want to join our team? It’s all just one step away (Literally, just a click on that link in your footer). Be outspoken. 
+</p>
+	<p>
+<br />
+
+
+<strong>Mounika Somisetty</strong>, &nbsp;&nbsp;Editor-in-chief
+</p>
+<p><center>
+<br />
+
+
+<br />
+“It's a jungle out there<br />
+Disorder and confusion everywhere<br />
+No one seems to care<br />
+Well I do<br />
+Hey, who's in charge here?”<br />
+</center>
 </p>
 </div>
 <?php get_sidebar(); ?>
