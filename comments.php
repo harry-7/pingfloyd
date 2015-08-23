24d0b2df05e8b6d9dedef79d37742b9653fb1b26
Nisarg Jhaveri <nisargjhaveri@gmail.com>
2015-08-23 19:20:48 +0530

    Make comments with Author name work
    - Also, fix for Anonymous authors

--- comments.php
+++ comments.php
@@ -13,7 +13,7 @@
 					&ldquo; <?php echo $comment->comment_content; ?> &rdquo;
 				</span>
 				<span class="comment_desc">
-					by <?php echo $comment->comment_author . ' on ' . get_comment_date() . ' at ' . get_comment_time(); ?>
+					<?php echo 'by ' . ($comment->comment_author ?: 'Anonymous') . ' on ' . get_comment_date() . ' at ' . get_comment_time(); ?>
 				</span>
 			</td>
 		</tr>
@@ -24,7 +24,7 @@
 			<td>
 				<form id="comment_form" action="<?php echo esc_url( home_url( '/wp-comments-post.php' ) ); ?>" method="POST">
 					<textarea id="comment" name="comment" aria-describedby="form-allowed-tags" aria-required="true" required="required" placeholder="What are your views on this?"></textarea>
-						<input type="text" name="name" id="commenter_name" placeholder="Your name" />
+						<input type="text" name="author" id="commenter_name" placeholder="Your name" />
 						<input name="submit" type="submit" id="submit" class="submit anim tag" value="Post Comment">
 						<input type="hidden" name="comment_post_ID" value="<?php echo get_the_ID(); ?>" id="comment_post_ID">
 						<input type="hidden" name="comment_parent" id="comment_parent" value="0">
